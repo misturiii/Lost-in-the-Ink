d8d1303f76595e00c0e10252d130b071e165a668
azyx0083 <azyx0083@gmail.com>
2024-09-23 01:08:42 -0400

    update scale and rotation

--- Assets/Scripts/Sticker.cs
+++ Assets/Scripts/Sticker.cs
@@ -6,22 +6,20 @@ public class Sticker : MonoBehaviour, IDragHandler, IDropHandler
     [SerializeField, Min(0)] float initialHeight = 5;
     [SerializeField, Min(0)] float sketchbookWidth = 40, sketchbookHeight = 30;  
     [SerializeField] GameObject prefab;
-    RectTransform rectTransform;
     Transform copy;
 
     void Start () {
-        rectTransform = GetComponent<RectTransform>();
         copy = null;
     }
 
     public void UseSticker() {
-        float x = -rectTransform.localPosition.x / Screen.width * sketchbookWidth;
-        float z = -rectTransform.localPosition.y / Screen.height * sketchbookHeight;
-        Debug.Log(rectTransform.localPosition.x);
+        float x = -transform.localPosition.x / Screen.width * sketchbookWidth;
+        float z = -transform.localPosition.y / Screen.height * sketchbookHeight;
         if (!copy) {
             copy = Instantiate(prefab).transform;
         }
         copy.localPosition = new Vector3(x, initialHeight, z);
+        copy.localEulerAngles = Vector3.zero;
     }
 
     public void OnDrop(PointerEventData eventData)
