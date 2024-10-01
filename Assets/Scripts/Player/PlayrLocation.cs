763d903e9b8b7d7d4525e8f3f4fbbc90d3d0761b
azyx0083 <azyx0083@gmail.com>
2024-10-01 14:04:21 -0400

    Add level

---
+++ Assets/Scripts/Player/PlayrLocation.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayrLocation : MonoBehaviour
+{
+    [SerializeField, Min(0)] float sketchbookWidth = 40, sketchbookHeight = 30;  
+    [SerializeField] float inventoryWidth = 400;
+
+    Transform player;
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    void OnEnable () {
+        float x = -(player.localPosition.x / sketchbookWidth) * (Screen.width - inventoryWidth);
+        float y = - (player.localPosition.z / sketchbookHeight) * Screen.height;
+        transform.position = new Vector2(x, y);
+    }
+}
