742e6087c13c18f9d7633d01ff15c811ec05e6bc
Christina0313 <christinalan20021023@gmail.com>
2024-10-02 15:51:25 -0400

    add airwall function

--- Assets/Scripts/Sticker/Sticker.cs
+++ Assets/Scripts/Sticker/Sticker.cs
@@ -7,10 +7,20 @@ public class Sticker : MonoBehaviour, IDragHandler, IDropHandler
     [SerializeField, Min(0)] float sketchbookWidth = 40, sketchbookHeight = 30;  
     [SerializeField] float inventoryWidth = 400;
     [SerializeField] GameObject prefab;
+    [SerializeField] GameObject airwall;
     Transform copy;
 
     void Start () {
         copy = null;
+        // Find the GameObject with the tag "airWall" and assign it to airwall
+        airwall = GameObject.FindWithTag("airWall");
+
+        // Check if the airwall was found
+        if (airwall == null)
+        {
+            Debug.LogError("Airwall object with tag 'airWall' not found in the scene!");
+        }
+        
     }
 
     public void UseSticker() {
@@ -26,10 +36,27 @@ public class Sticker : MonoBehaviour, IDragHandler, IDropHandler
     public void OnDrop(PointerEventData eventData)
     {
         UseSticker();
+        CheckAndDisableAirwall();
+
+        
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         transform.position += (Vector3)eventData.delta;
+
     }
+
+    private void CheckAndDisableAirwall()
+    {
+        if (gameObject.CompareTag("key")) // Assuming the sticker is tagged as "Key"
+        {
+            if (airwall != null && airwall.activeSelf)
+            {
+                airwall.SetActive(false); // Disable the airwall
+                Debug.Log("Airwall disabled because the 'Key' sticker was used.");
+            }
+        }
+    }
+
 }
