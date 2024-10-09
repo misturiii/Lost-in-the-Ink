5ac35f9274545ff2fdda0f736e85a857cda53b73
Christina0313 <christinalan20021023@gmail.com>
2024-10-09 04:44:42 -0400

    applying the new map, create stickers using the done prefabs

--- Assets/Scripts/TriggerEvent/InventoryToggle.cs
+++ Assets/Scripts/TriggerEvent/InventoryToggle.cs
@@ -5,6 +5,7 @@ public class InventoryToggle : MonoBehaviour
 {
     public GameObject inventoryPanel;  // Reference to the inventory panel
     public GameObject inventoryPanel2;  // Reference to the second inventory panel (if any)
+    // public GameObject inventoryChat;
 
     private bool isInventoryOpen = false;  // Track whether the inventory is open
     InputActions inputActions;
@@ -29,6 +30,7 @@ public class InventoryToggle : MonoBehaviour
         isInventoryOpen = !isInventoryOpen;  // Toggle the inventory state
         inventoryPanel.SetActive(isInventoryOpen);  // Show or hide the panel based on the state
         inventoryPanel2.SetActive(isInventoryOpen);  // Show or hide the panel based on the state
+        // inventoryChat.SetActive(isInventoryOpen);
 
         if (isInventoryOpen)
         {
